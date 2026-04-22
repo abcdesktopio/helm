82b107f7d2df442c2fadf86fc616269d9d90610d
jfv <jeanfrancois.vincent@orange.com>
2026-04-22 14:23:29 +0200

    Add support of install helm video generation

---
+++ scripts/generate-video.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+VERSION=$(grep appVersion "charts/abcdesktop/Chart.yaml" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+
+echo "NAMESPACE=abcdesktop"
+NAMESPACE=abcdesktop
+sleep 1
+echo "helm repo add abcdesktop https://abcdesktopio.github.io/helm/"
+helm repo add abcdesktop https://abcdesktopio.github.io/helm/
+sleep 1
+echo "helm install my-abcdesktop abcdesktop/abcdesktop --version ${VERSION} --create-namespace -n \${NAMESPACE}"
+helm install my-abcdesktop abcdesktop/abcdesktop --version ${VERSION} --create-namespace -n ${NAMESPACE}
+sleep 5
